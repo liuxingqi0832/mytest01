e4a1d8aa3b1bd4fae4341d9390ca3e76786a6431
liu <595926113@qq.com>
2021-09-23 06:59:50 +0800

    init

---
+++ src/main/java/com/zgall/pojo/User.java
@@ -0,0 +1,15 @@
+package com.zgall.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    protected String name;
+    private Integer age;
+}
